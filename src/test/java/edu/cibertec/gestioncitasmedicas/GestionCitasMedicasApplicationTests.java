de5a50acbb74ecf6294550ce923bf144f9057b80
ncmendoza <nelzoncriollo@gmail.com>
2023-05-01 17:57:52 -0500

    se agrega el proyecto

---
+++ src/test/java/edu/cibertec/gestioncitasmedicas/GestionCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.gestioncitasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCitasMedicasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
