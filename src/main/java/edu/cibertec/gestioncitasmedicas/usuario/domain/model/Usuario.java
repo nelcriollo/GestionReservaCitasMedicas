ddae84c045aa932f11dabedd0856fa7f31ad7bb8
ncmendoza <nelzoncriollo@gmail.com>
2023-05-08 01:39:25 -0500

    se crea la configuracion del proyecto

---
+++ src/main/java/edu/cibertec/gestioncitasmedicas/usuario/domain/model/Usuario.java
@@ -0,0 +1,34 @@
+package edu.cibertec.gestioncitasmedicas.usuario.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_Usuario", nullable = false)
+    private long Id_Usuario;
+
+    @Column(name = "Nombre", length =50 ,nullable = false)
+    private String Nombre;
+
+    @Column(name = "Apellidos", length =70 ,nullable = false)
+    private String Apellidos;
+
+    @Column(name = "Email", length =150, nullable = false)
+    private String Email;
+
+    @Column(name = "Password", length = 250, nullable = false)
+    private String Password;
+
+    @Column(name = "Estado", nullable = false)
+    private int Estado;
+}
